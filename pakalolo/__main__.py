1a98f61f6bbc49e49d2332906f3da914166779a8
Marios Isaakidis <misaakidis@yahoo.gr>
2017-01-01 20:37:19 +0200

    Use click package for parsing command line options; Let users set the keyring dir

--- pakalolo/__main__.py
+++ pakalolo/__main__.py
@@ -1,13 +1,31 @@
-from os.path import expanduser
+import click
+import os
+import sys
 import gnupg
 
-def main(args=None):
-    print("Pakalolo key manager")
+gpg = None
 
-def make_keying():
-    # Create a keyring under ~/.pakalolo
-    home = expanduser("~")
-    gpg = gnupg.GPG(homedir=home + '/.pakalolo')
+@click.command()
+@click.option('--homedir', default=None, envvar='PAKALOLOHOME', help="set the directory to store the pakalolo keyring (default is ~/.pakalolo/)")
+def main(homedir):
+    """pakalolo key management tool"""
+    open_keying(homedir)
+
+
+def open_keying(homedir=None):
+    """Open the pakalolo keyring"""
+    if homedir is None:
+        homedir = os.path.expanduser("~") + '/.pakalolo'
+    if not os.path.exists(homedir):
+        try:
+            os.makedirs(homedir)
+        except OSError as exception:
+            sys.exit("pakalolo cannot create the directory: " + homedir)
+    if not os.access(homedir, os.W_OK):
+        sys.exit("pakalolo has no write access to the directory: " + homedir)
+    global gpg
+    gpg = gnupg.GPG(homedir=homedir)
+
 
 if __name__ == "__main__":
     main()
