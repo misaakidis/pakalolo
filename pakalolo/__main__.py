1b032cb3f0be4b6bba88121915bb751ab06eeb23
Marios Isaakidis <misaakidis@yahoo.gr>
2017-01-05 22:14:15 +0200

    Refactor code to not use global variables

--- pakalolo/__main__.py
+++ pakalolo/__main__.py
@@ -5,29 +5,24 @@ import gnupg
 import pickle
 from pakalolo import claimchain, __version__
 
-gpg = None
-storedir = None
-chain = None
-chain_updated = False
-
 
 @click.group(invoke_without_command=True)
 @click.option('--homedir', default=None, envvar='PAKALOLOHOME', type=click.Path(exists=False),
               help="set the keyring directory (default is ~/.pakalolo/)")
 def cli(homedir):
     """pakalolo key management tool"""
-    open_keying(homedir)
-    global chain
-    chain = load_chain('pakalolo')
+    chain_updated = False
+    storedir, gpg = open_keying(homedir)
+    chain = load_chain(storedir)
     if chain is None:
         if click.confirm("No claimchain was found in directory " + storedir +
                                  ", would you like to create one?", abort=True):
-            chain = first_run()
+            chain, chain_updated = first_run(gpg)
     if len(chain.store) == 0:
         if click.confirm("Your claimchain contains no keys, would you like to create one?", abort=True):
-            chain = first_run()
+            chain, chain_updated = first_run(gpg)
     if chain_updated and click.confirm('Do you want to save your changes?', abort=True):
-        store_chain('pakalolo')
+        store_chain(chain, storedir)
 
 
 @cli.command()
@@ -36,15 +31,14 @@ def version():
     sys.exit('pakalolo ' + __version__)
 
 
-def first_run():
+def first_run(gpg):
     print "Welcome to pakalolo"
     print "generating your first key..."
-    alice_key = gen_key()
+    alice_key = gen_key(gpg)
     chain = claimchain.Chain()
     chain.multi_add([alice_key.fingerprint])
-    global chain_updated
     chain_updated = True
-    return chain
+    return chain, chain_updated
 
 
 def open_keying(homedir=None):
@@ -58,13 +52,11 @@ def open_keying(homedir=None):
             sys.exit("pakalolo cannot create the directory: " + homedir)
     if not os.access(homedir, os.W_OK):
         sys.exit("pakalolo has no write access to the directory: " + homedir)
-    global gpg
-    global storedir
-    storedir = homedir
     gpg = gnupg.GPG(homedir=homedir)
+    return homedir, gpg
 
 
-def gen_key():
+def gen_key(gpg):
     alice = {'name_real': 'Alice',
              'name_email': 'alice@inter.net',
              'expire_date': '2017-04-01',
@@ -80,12 +72,12 @@ def gen_key():
     return alice_key
 
 
-def store_chain(store_file):
+def store_chain(chain, storedir, filename='pakalolo'):
     """Save the claimchaine store in the homedir"""
-    save_pickle(chain.store, '/' + store_file)
+    save_pickle(chain.store, storedir, filename)
 
 
-def load_chain(store_file):
+def load_chain(storedir, filename='pakalolo'):
     """
     Restore a claimchain from a saved store
 
@@ -93,21 +85,21 @@ def load_chain(store_file):
             not exist or cannot be loaded.
     """
     try:
-        store = load_pickle(store_file)
+        store = load_pickle(storedir, filename)
         chain = claimchain.Chain(store)
     except:
         return None
     return chain
 
 
-def save_pickle(obj, name):
-    path = os.path.join(storedir + name + '.pkl')
+def save_pickle(obj, dir, filename):
+    path = os.path.join(dir + filename + '.pkl')
     with open(path, 'wb') as f:
         pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
 
 
-def load_pickle(name):
-    path = os.path.join(storedir + name + '.pkl')
+def load_pickle(dir, filename):
+    path = os.path.join(dir + filename + '.pkl')
     with open(path, 'rb') as f:
         return pickle.load(f)
 
